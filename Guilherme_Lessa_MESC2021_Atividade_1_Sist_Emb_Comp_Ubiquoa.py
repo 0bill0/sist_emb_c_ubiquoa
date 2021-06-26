cc5e9bc2fce17a4fb5eab16b7c621e6e2407f57d
Guilherme Lessa <guilhermelessa@Guilherme-Lessa.local>
2021-06-26 20:05:34 -0300

    first commit

---
+++ Guilherme_Lessa_MESC2021_Atividade_1_Sist_Emb_Comp_Ubiquoa.py
@@ -0,0 +1,10 @@
+#coding: utf-8
+#
+def word_counts(text):
+	return 
+
+if __name__ == "__main__":
+	print("Entre com o texto e receba o número de palavras:")
+	text = raw_input()
+	output = word_counts(text)
+	print output
