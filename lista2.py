3e01b50b99b6f4f17054b68db72aacf4239f600b
Guilherme Luiz Lessa <lessaguigui@hotmail.com>
2021-06-27 10:32:20 -0300

    Lista 2

---
+++ lista2.py
@@ -0,0 +1,13 @@
+#coding: utf-8
+""" 
+1) Encontre em um texto os nomes próprios e os retorne em uma lista. Utilize o Regex (‘import re’) e a função findall(). Na versão básica, retorne todas as palavras que iniciam com maiúscula.
+
+
+2) Apresente um plot de alguns segundos dos dados de acelerômetro do dataset:
+https://archive.ics.uci.edu/ml/datasets/Activity+Recognition+from+Single+Chest-Mounted+Accelerometer#
+Use a função read_csv() para abrir os arquivos
+
+"""
+
+if __name__ == "__main__":
+	pass
